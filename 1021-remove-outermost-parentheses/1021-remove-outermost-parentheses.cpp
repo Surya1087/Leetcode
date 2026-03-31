9e54954739a65863e3aa017b3cf8b0ace17465d7
Surya Pratap Singh <98077479+Surya1087@users.noreply.github.com>
2026-04-01 00:34:27 +0530

    Time: 3 ms (18.33%), Space: 8.9 MB (54.34%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string removeOuterParentheses(string S) {
+        string result = "";
+        stack<char> st;
+        for(char ch: S) {
+            if(ch == '('){
+                if(!st.empty()) result += ch;
+                st.push(ch);
+            }
+            else{
+                st.pop();
+                if(!st.empty())result += ch;
+            }
+        }
+        return result;
+    }
+};
+
+//         string res;
+//         int opened = 0;
+//         for (char c : S) {
+//             if (c == '(' && opened++ > 0) res += c;
+//             if (c == ')' && opened-- > 1) res += c;
+//         }
+//         return res;
+//     }
+// };
