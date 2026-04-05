b0628cb9b7c01e8e6e8b3e85af153cb4e1ab8a05
Surya Pratap Singh <98077479+Surya1087@users.noreply.github.com>
2026-04-05 21:33:38 +0530

    Time: 4 ms (38.32%), Space: 16.9 MB (12.51%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        auto cmp = [](const pair<char, int>& a, const pair<char, int>& b) {
-            return a.second < b.second;
-        };
-        
-        priority_queue<pair<char, int>, vector<pair<char, int>>, decltype(cmp)> pq(cmp);
-        
-        unordered_map<char, int> hm;
-        
-        for (char c : s) {
-            hm[c]++;
-        }
-        
-        for (const auto& entry : hm) {
-            pq.push(make_pair(entry.first, entry.second));
-        }
-        
-        string result = "";
-        while (!pq.empty()) {
-            pair<char, int> p = pq.top();
-            pq.pop();
-            result.append(p.second, p.first);
-        }
-        
-        return result;
+    unordered_map<char, int> freq;
+
+    
+    for (char c : s) {
+        freq[c]++;
     }
+
+    int n = s.size();
+
+   
+    vector<vector<char>> bucket(n + 1);
+
+    
+    for (const auto &p : freq) {
+        bucket[p.second].push_back(p.first);
+    }
+
+    
+    string ans;
+    ans.reserve(s.size());
+
+    for (int i = n; i >= 1; --i) {
+        for (char c : bucket[i]) {
+            ans += string(i, c);
+        }
+    }
+
+    return ans;
+}
 };
-
-
