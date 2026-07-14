dd4fe02796264bfeff84530fa8a06e1300cdd6b4
Surya Pratap Singh <98077479+Surya1087@users.noreply.github.com>
2026-07-14 22:29:44 +0530

    Time: 0 ms (100%), Space: 14.8 MB (55.29%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n=nums.size();
-        if(n<2||n>10000){
-            return {};
-        }
-        else{
-            unordered_map<int, int> numToIndex;
-            for(int i=0;i<n;i++){
-                int complement=target-nums[i];
-                if(numToIndex.find(complement)!=numToIndex.end()){
-                    return {numToIndex[complement],i};
-                }
-                numToIndex[nums[i]]=i;
-                    
-                }
+        unordered_map<int, int> numMap;
+        int n = nums.size();
 
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement)) {
+                return {numMap[complement], i};
             }
-            return{};
+            numMap[nums[i]] = i;
         }
-        
-        
-    };
-    
+
+        return {}; 
+    }
+};
